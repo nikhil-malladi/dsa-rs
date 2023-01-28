52a89b8573f166c544dede958227be05fe0952bd
Nikhil-Malladi <malladi.vsnikhil@gmail.com>
2023-01-29 00:32:26 +0530

    added is_palindrome problem from leetcode

--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -1,7 +1,9 @@
 mod contains_duplicate;
 mod is_anagram;
 mod two_sum;
+mod is_palindrome;
 
 pub use self::contains_duplicate::contains_duplicate_1;
 pub use self::is_anagram::{is_anagram_1,is_anagram_2};
 pub use self::two_sum::two_sum_1;
+pub use self::is_palindrome::is_palindrome_1;
